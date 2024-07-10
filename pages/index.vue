ac3186ed185d2c192f24c5379f6125252d62f2a8
Muh Jumri Yadi <111132247+JumriYadi@users.noreply.github.com>
2024-07-10 14:44:12 +0700

    Create index.vue

---
+++ pages/index.vue
@@ -0,0 +1,205 @@
+<template>
+  <div class="w3-content w3-margin-top" style="max-width: 1400px">
+    <!-- The Grid -->
+    <div class="w3-row-padding">
+      <!-- Left Column -->
+      <div class="w3-third">
+        <div class="w3-white w3-text-grey w3-card-4">
+          <div class="w3-display-container">
+            <img src="/jumri.jpg" style="width: 100%" alt="Avatar" />
+            <div class="w3-display-bottomleft w3-container w3-text-black">
+              <h2>Muh Jumri Yadi</h2>
+            </div>
+          </div>
+          <div class="w3-container">
+            <p>
+              <i class="fa fa-briefcase fa-fw w3-margin-right w3-large w3-text-teal"></i
+              >Data Scientist
+            </p>
+            <p>
+              <i class="fa fa-home fa-fw w3-margin-right w3-large w3-text-teal"></i
+              >Jakarta, Indonesia
+            </p>
+            <p>
+              <i class="fa fa-envelope fa-fw w3-margin-right w3-large w3-text-teal"></i>
+              <a href="mailto:Jumriyadi11@gmail.com">Jumriyadi11@gmail.com</a>
+            </p>
+            <p>
+              <i class="fa fa-phone fa-fw w3-margin-right w3-large w3-text-teal"></i>(+62)
+              85216859793
+            </p>
+            <p>
+              <i class="fa fa-linkedin fa-fw w3-margin-right w3-large w3-text-teal"></i
+              ><a href="https://www.linkedin.com/in/muhjumriyadi/"
+                >https://www.linkedin.com/in/muhjumriyadi/</a
+              >
+            </p>
+            <hr />
+
+            <p class="w3-large">
+              <b
+                ><i class="fa fa-asterisk fa-fw w3-margin-right w3-text-teal"></i
+                >Skills</b
+              >
+            </p>
+            <p>Communication Skills</p>
+            <div class="w3-light-grey w3-round-xlarge w3-small">
+              <div
+                class="w3-container w3-center w3-round-xlarge w3-teal"
+                style="width: 90%"
+              >
+                90%
+              </div>
+            </div>
+            <p>Teamwork and Collaboration</p>
+            <div class="w3-light-grey w3-round-xlarge w3-small">
+              <div
+                class="w3-container w3-center w3-round-xlarge w3-teal"
+                style="width: 80%"
+              >
+                <div class="w3-center w3-text-white">80%</div>
+              </div>
+            </div>
+            <p>Adaptability</p>
+            <div class="w3-light-grey w3-round-xlarge w3-small">
+              <div
+                class="w3-container w3-center w3-round-xlarge w3-teal"
+                style="width: 75%"
+              >
+                75%
+              </div>
+            </div>
+            <p>Time Management</p>
+            <div class="w3-light-grey w3-round-xlarge w3-small">
+              <div
+                class="w3-container w3-center w3-round-xlarge w3-teal"
+                style="width: 50%"
+              >
+                50%
+              </div>
+            </div>
+            <br />
+
+            <p class="w3-large w3-text-theme">
+              <b
+                ><i class="fa fa-globe fa-fw w3-margin-right w3-text-teal"></i
+                >Languages</b
+              >
+            </p>
+            <p>Indonesia</p>
+            <div class="w3-light-grey w3-round-xlarge">
+              <div
+                class="w3-round-xlarge w3-teal"
+                style="height: 24px; width: 100%"
+              ></div>
+            </div>
+            <p>English</p>
+            <div class="w3-light-grey w3-round-xlarge">
+              <div class="w3-round-xlarge w3-teal" style="height: 24px; width: 55%"></div>
+            </div>
+            <br />
+          </div>
+        </div>
+        <br />
+
+        <!-- End Left Column -->
+      </div>
+
+      <!-- Right Column -->
+      <div class="w3-twothird">
+        <div class="w3-container w3-card w3-white w3-margin-bottom">
+          <h2 class="w3-text-grey w3-padding-16">
+            <i class="fa fa-user fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>About
+            Me
+          </h2>
+          <div class="w3-container">
+            <ul>
+              <li>Technology and Innovation are the Future</li>
+              <li>6th Semester Computer Systems Student at Bung Karno University</li>
+              <li>Specializing in Data Science</li>
+              <li>Ready to Contribute to Challenging Tech Projects</li>
+            </ul>
+          </div>
+        </div>
+        <div class="w3-container w3-card w3-white w3-margin-bottom">
+          <h2 class="w3-text-grey w3-padding-16">
+            <i class="fa fa-suitcase fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i
+            >Experience
+          </h2>
+          <div class="w3-container">
+            <h5 class="w3-opacity"><b>Juni 2024</b></h5>
+            <p>Belajar Machine Learning untuk Pemula – Dicoding</p>
+            <hr />
+          </div>
+          <div class="w3-container">
+            <h5 class="w3-opacity"><b>Mei 2024</b></h5>
+            <p>
+              Advanced Uncerstanding of Basic Machine Learning and Data Science Using
+              Python: This program uqips me with the necessary skills to preproccess data,
+              build and evaluate machine learning models, and perform data analysis using
+              Python. Through a combination of theoretical learning and practial coding
+              exercises, I have develop a comprehensive skill set that allows me to
+              approach and solve data-drivem problems with confidence and efficiency.
+            </p>
+            <hr />
+          </div>
+        </div>
+
+        <div class="w3-container w3-card w3-white">
+          <h2 class="w3-text-grey w3-padding-16">
+            <i class="fa fa-certificate fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i
+            >Education
+          </h2>
+          <div class="w3-container">
+            <h5 class="w3-opacity"><b>Universitas Bung Karno</b></h5>
+            <h6 class="w3-text-teal">
+              <i class="fa fa-calendar fa-fw w3-margin-right"></i>2021 – Present
+            </h6>
+            <p>Computer Systems</p>
+            <hr />
+          </div>
+          <div class="w3-container">
+            <h5 class="w3-opacity"><b>SMK YP IPPI PETOJO </b></h5>
+            <h6 class="w3-text-teal">
+              <i class="fa fa-calendar fa-fw w3-margin-right"></i>2018 – 2021
+            </h6>
+            <p>Computer and Network Engineering</p>
+            <hr />
+          </div>
+        </div>
+
+        <!-- End Right Column -->
+      </div>
+
+      <!-- End Grid -->
+    </div>
+
+    <!-- End Page Container -->
+  </div>
+
+  <footer class="w3-container w3-teal w3-center w3-margin-top">
+    <p>Find me on social media.</p>
+    <i class="fa fa-facebook-official w3-hover-opacity"></i>
+    <i class="fa fa-instagram w3-hover-opacity"></i>
+    <i class="fa fa-snapchat w3-hover-opacity"></i>
+    <i class="fa fa-pinterest-p w3-hover-opacity"></i>
+    <i class="fa fa-twitter w3-hover-opacity"></i>
+    <i class="fa fa-linkedin w3-hover-opacity"></i>
+    <p>
+      Powered by
+      <a href="https://www.w3schools.com/w3css/default.asp" target="_blank">w3.css</a>
+    </p>
+  </footer>
+</template>
+<style scoped>
+html,
+body,
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  font-family: "Roboto", sans-serif;
+}
+</style>
