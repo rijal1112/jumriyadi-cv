f107562b3bd7dcdac30cf1ff8eb54db04d8cb5d6
Muh Jumri Yadi <111132247+JumriYadi@users.noreply.github.com>
2024-07-10 14:47:59 +0700

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,6 +7,7 @@ export default defineNuxtConfig({
   },
   app: {
     head: {
+      title: "Muh Jumri Yadi Curriculum Vitae",
       link: [
         { rel: "stylesheet", href: "https://www.w3schools.com/w3css/4/w3.css" },
         { rel: "stylesheet", href: "https://fonts.googleapis.com/css?family=Roboto" },
